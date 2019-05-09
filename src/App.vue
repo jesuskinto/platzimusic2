9daefdbbb59cd863c2f899c9b8b9b7a82d39a739
Jesus Quinto <jesuskinto@gmail.com>
2019-05-09 20:14:22 +0000

    todolist

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,113 @@
 <template lang="pug">
   #app
     section.section
-      nav.navbar
-        .field.has-addons
-          .control.is-expanded
-            input.input(
-              type="text"
-              placeholder="Buscar canciones"
-              v-model="nombre")
-          .control
-            button.button.is-info(@click="buscar") Buscar
-          .control
-            button.button.is-danger &times;
-          .control
-            button.button
-              span.is-size-7 Encontrado: {{ cantidad }}
-
-      .container.custom
-        .columns
-          .colum(v-for="c in canciones") {{ c.nombre }},{{ c.artista }}
+      .container
+        h1.title Lista de tareas
+        .box
+          h3.subtitle
+            i {{ name }}
+          .box(v-for="(t,i) in tasks")
+            span {{ t.title }} | {{ t.time }}
+            button.button.is-danger.is-small.is-x(
+              @click="removeTask(i)"
+            ) &times;
+          .isEmpty(v-show="isEmpty")
+            p ¡No tiene ninguna tarea #[strong 🙁] !
+            p Presione #[span agregar] para crear una nueva tarea
+          form.field.has-addons(@submit.prevent="addTask")
+            .control.is-expanded
+              input.input(
+                v-model="newTask.title"
+                type="text"
+                placeholder="Titulo"
+                required
+              )
+            .control
+              input.input(
+                v-model="newTask.time"
+                type="time"
+                required
+              )
+            .control
+              button.button.is-primary(type="submit") Agregar
+        p Total de horas: #[strong {{ total }}]
 </template>
 
 <script>
-
-const canciones = [
-  { 'nombre': 'De mes en mes', 'artista': 'Ricardo Arjona' },
-  { 'nombre': 'Pinguinos en la cama', 'artista': 'Ricardo Arjona' },
-  { 'nombre': 'Atrevete', 'artista': 'Calle 13' },
-  { 'nombre': 'Nadie como tu', 'artista': 'Calle 13' }
-]
-
 export default {
   name: 'app',
   data () {
     return {
-      canciones: [],
-      nombre: ''
+      name: 'Titulo',
+      tasks: [],
+      newTask: {
+        title: null,
+        time: null
+      }
+    }
+  },
+  created () {
+    const tasks = localStorage.getItem('tasks')
+    console.log(tasks)
+    if (tasks) {
+      this.tasks = JSON.parse(tasks)
     }
   },
   methods: {
-    buscar () {
-      this.canciones = canciones
+    setTasks () {
+      localStorage.setItem('tasks', JSON.stringify(this.tasks))
+    },
+    addTask () {
+      const newTask = JSON.parse(JSON.stringify(this.newTask))
+      this.tasks.push(newTask)
+      this.setTasks()
+      this.clean()
+    },
+    clean () {
+      this.newTask = {
+        title: null,
+        time: null
+      }
+    },
+    removeTask (id) {
+      this.tasks.splice(id, 1)
+      this.setTasks()
     }
   },
   computed: {
-    cantidad () {
-      return this.canciones.length
+    total () {
+      let hours = 0
+      let minuts = 0
+      this.tasks.forEach(task => {
+        const time = task.time.split(':')
+        hours += parseInt(time[0])
+        minuts += parseInt(time[1])
+      })
+      return `${hours}:${minuts} `
+    },
+    isEmpty () {
+      if (this.tasks.length) {
+        return false
+      }
+      return true
     }
   }
 }
 </script>
 
-<style lang="scss">
-@import 'scss/main.scss';
-
-#app {
-  .custom {
-    margin-top: 30px;
-  }
-}
+<style lang="sass">
+@import './sass/main.sass'
+#app
+  .container
+    max-width: 600px
+  .titulo
+    width: 50%
+  .custom
+    margin-top: 30px
+  .is-x
+    float: right
+  .isEmpty
+    text-align: center
+    font-size: 20px
+    margin-bottom: 25px
 </style>
